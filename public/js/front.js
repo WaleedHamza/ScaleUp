5c98905efe8e91bc5f40b0174bd6f1df4df42eaf
Jeannie Rose <33160792+honestgraphics@users.noreply.github.com>
2018-06-23 16:37:28 -0400

    Update front.js

--- public/js/front.js
+++ public/js/front.js
@@ -1,57 +1,62 @@
 // from view.js example in classwork 8 squelize CRUD  
 $(document).ready(function () {
-      $('select').formSelect();
-      $('.grid').isotope({
-        // options
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows'
-      });
+  //Materialize Select 
+  $('select').material_select();
+  $('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
 
-      $('#addDataBtn').on('click', function () {
-        insertZone();
-        console.log(' i have been clicked', zone);
-        location.reload();
-      });
+  $(document).on("submit", "#form", insertZone);
+});
 
-      function insertZone() {
+$('#addDataBtn').on('click', function () {
+  insertZone();
+  console.log(' i have been clicked', zone);
+  // $("#building option:first").prop('disabled', true);
+  // $("#building option:first").attr('selected', true);
+  // $('select').material_select();
+});
 
-        //making inputs work...
-        var zone = {
-          // building name
-          buildingName: $("#building").val().trim(),
-          // plant zones
-          plant_zones: $("#zone").val().trim(),
-          // ELectricity 
-          utility1capacity: $("#electricCap").val().trim(),
-          utility1load: $("#electricUsed").val().trim(),
-          // Water
-          utility2capacity: $("#waterCap").val().trim(),
-          utility2load: $("#waterUsed").val().trim(),
-          //HVAC
-          utility3capacity: $("#hvacCap").val().trim(),
-          utility3load: $("#hvacUsed").val().trim(),
-          // Steam
-          utility4capacity: $("#steamCap").val().trim(),
-          utility4load: $("#steamUsed").val().trim(),
-        }
+function insertZone() {
 
-        console.log('this is the zone var front.js', zone);
+  //making inputs work...
+  var zone = {
+    // building name
+    buildingName: $("#building").val().trim(),
+    // plant zones
+    plant_zones: $("#zone").val().trim(),
+    // ELectricity 
+    utility1capacity: $("#electricCap").val().trim(),
+    utility1load: $("#electricUsed").val().trim(),
+    // Water
+    utility2capacity: $("#waterCap").val().trim(),
+    utility2load: $("#waterUsed").val().trim(),
+    //HVAC
+    utility3capacity: $("#hvacCap").val().trim(),
+    utility3load: $("#hvacUsed").val().trim(),
+    // Steam
+    utility4capacity: $("#steamCap").val().trim(),
+    utility4load: $("#steamUsed").val().trim(),
+  }
 
-        //clear inputs
-        $("#building").val("");
-        $("#zone").val("");
-        $("#electricUsed").val("");
-        $("#electricCap").val("");
-        $("#waterUsed").val("");
-        $("#waterCap").val("");
-        $("#hvacUsed").val("");
-        $("#hvacCap").val("");
-        $("#steamUsed").val("");
-        $("#steamCap").val("");
+  console.log('this is the zone var front.js', zone);
+
+  //clear inputs
+  $("#building").val("");
+  $("#zone").val("");
+  $("#electricUsed").val("");
+  $("#electricCap").val("");
+  $("#waterUsed").val("");
+  $("#waterCap").val("");
+  $("#hvacUsed").val("");
+  $("#hvacCap").val("");
+  $("#steamUsed").val("");
+  $("#steamCap").val("");
 
 
-        $.post("/api/data", zone).done(function (data) {
-          console.log(data);
-        });
-      }
-    });
+  $.post("/api/data", zone).done(function (data) {
+    console.log(data);
+  });
+}
